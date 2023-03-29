2a449bf2bc7881c8f77fe0ba5429d67e8ec80d57
Abhishek Anand <anand2000ys@gmail.com>
2023-03-29 11:18:38 +0530

    backend function added

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -7,7 +8,17 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+  try {
+    const r = await axios.HttpStatusCode(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { header: { "Private-Key": "95d01606-7eef-4bb3-a05d-debe4c665e8b" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+  //   return res.json({ username: username, secret: "sha256..." });
 });
 
 app.listen(3001);
