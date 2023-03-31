ead444df3ee045296932bfa7c2ea59d06b18c80f
Abhishek Anand <anand2000ys@gmail.com>
2023-03-31 09:52:31 +0530

    chatpage function added

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,15 @@
-const ChatsPage = () => {
-  return <div className="background">Chats</div>;
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div className="background">
+      <PrettyChatWindow
+        projectId={import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID}
+        username={props.user.username}
+        secret={props.user.secret}
+      />
+    </div>
+  );
 };
 
 export default ChatsPage;
